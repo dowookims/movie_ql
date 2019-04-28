1cf5a94c99cc884be40990c82c53a8d2a6ffdae6
dowookims <kaei2574@gmail.com>
2019-04-28 12:38:32 +0900

    add axios to change fetch to axios and make some funcs :detail and getSuggestion

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,12 @@
-import { getMovies } from './db';
+import { getMovies, getMovie, getSuggestions } from './db';
 
 
 const resolvers = {
   Query: {
     movies: (_, {limit, rating}) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
-};
+};;
 
 export default resolvers;
